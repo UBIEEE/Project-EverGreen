f0f7762d08bb51e6993ddf949dbdfeb55aed7cf1
Alex M <apmchugh@buffalo.edu>
2024-10-25 11:25:16 -0400

    remove now-unnecessary models

--- evergreen/eg_app/models.py
+++ evergreen/eg_app/models.py
@@ -1,12 +1,3 @@
 from django.db import models
 
 # Create your models here.
-
-class Account(models.Model):
-    email = models.EmailField(unique=True)
-    pwdHash = models.BinaryField()
-
-
-class AuthToken(models.Model):
-    token = models.TextField()
-    user = models.ForeignKey(Account, on_delete=models.CASCADE)
