d183a1ed33c42e8d0cc7e2f534896000aa378cc3
Gianna Germain <ggermain@buffalo.edu>
2024-10-26 00:01:14 -0400

    I thought I fixed my repo but it seems not. I am pushing to ensure progress is saved. I will send update once I am able to test it myself if no one gets to it before me

--- evergreen/staticfiles/js/functions.js
+++ evergreen/staticfiles/js/functions.js
@@ -4,68 +4,15 @@ feedPost = {}
 function start() {
 	document.getElementById("about").innerHTML += "<br/> We have Javascript, too! 😀"
 	document.getElementById("about").innerHTML += "<br/> This probably all gonna be replaced anyway idk"
-
-	updateFeed();
-	setInterval(updateFeed, 1000);
-}
- 
-function updateFeed() { //updates both posts and comments
-	const request = new XMLHttpRequest();
-	request.open("GET", "updateFeed");
-	request.send();
 }
 
-function updatePosts_Feed(serverPost) {
-	
+
+function login() {
+	document.getElementById("loggedin").innerHTML = '<form action="/logout" method="post" enctype="application/x-www-form-urlencoded">{% csrf_token %}<input type="submit" value="Logout"></form>'
 }
 
-function uploadPost() {
-	const postImage = document.getElementBy("postImage")
-	const img = postImage.value
-	postImage.value = "";
-
-	const postCaption = document.getElementById("postCaption")
-	const cap = postCaption.value
-	postCaption.value = "";
-
-	const postJSON = {"image":img, "caption":cap}
-	request.open("POST", "uploadPost");
-	request.send(JSON.stringify(postJSON));
+function logout() {
+	document.getElementById("loggedin").innerHTML = '<form action="/logout" method="post" enctype="application/x-www-form-urlencoded">{% csrf_token %}<input type="submit" value="Logout" disabled></form>'
 }
 
-function addPostToFeed() {
-
-}
-
-function deletePost() {
-	const request = new XMLHttpRequest();
-	request.open("DELETE", "deletePost");
-	request.send();
-}
-
-function deleteComment(commentId) {
-	const request = new XMLHttpRequest();
-	request.open("DELETE", "deleteComment");
-	request.send();
-}
-
-function post_HTML() {
-
-}
-
-function comment_HTML() {
-
-}
-
-// function likeButton_HTML() {
-// 	document.getElementsByClassName("likeButton").innerHTML = '<form action="dislikePost" method="post" enctype="application/x-www-form-urlencoded">{{post.likes}}<button id="dislike_button" onclick="dislikeButton_HTML()">Un-Like</button></label>'
-// }
-
-// function dislikeButton_HTML() {
-// 	document.getElementsByClassName("likeButton").innerHTML = '<form action="likePost" method="post" enctype="application/x-www-form-urlencoded">{{post.likes}}<button id="like_button" onclick="likeButton_HTML()">Like</button></label>'
-// }
-
-function initWS() {
-
-}
 
