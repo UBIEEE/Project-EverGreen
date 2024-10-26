82c64af1fe0f08d1ba59b5e0dfe912eb54879a69
Gianna Germain <ggermain@buffalo.edu>
2024-10-26 14:42:33 -0400

    DO NOT MERGE: Use as source code for chat functionality

--- evergreen/eg_app/views.py
+++ evergreen/eg_app/views.py
@@ -1,10 +1,8 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
 import eg_app.util.validators as val
-
 from django.http import JsonResponse
-
 from django.views.decorators.csrf import csrf_exempt
+from eg_app.models import Post, Comments
 
 # Create your views here.
 
@@ -15,12 +13,8 @@ def index(request):
 @csrf_exempt
 def validate(request):
     if request.method == "POST":
-
-
         password = request.POST.get("password")
         email = request.POST.get("email")
-
-
 
         valid_pass = True
         valid_email = True
@@ -31,5 +25,61 @@ def validate(request):
         if not val.validate_email(email):
             valid_email = False
 
-
         return JsonResponse({"valid_pass":str(valid_pass),"valid_email":str(valid_email)})
+
+def updateFeed(request):
+    posts = Post.objects.all().order_by('timestamp')
+    comments = Comments.objects.all().order_by('timestamp')
+    return render(request,'index.html', {"posts": posts, "comments": comments})
+
+def uploadPost(request):
+    # user = request.user.email
+    user = 'guest@buffalo.edu'
+    image = request.FILES.get("image upload")
+    caption = request.POST["caption"]
+
+    post = Post.objects.create(user=user, image=image, caption=caption)
+    post.save()
+    return redirect("/")
+
+def deletePost(request, pk):
+    post = Post.objects.get(pk=pk)
+    
+    if post.user == request.user:
+        post.delete()
+    return redirect("/")
+
+def likePost(request, pk):
+    post = Post.objects.get(pk=pk)
+    
+    if not post.userLikes.contains(request.user):
+        post.likes += 1
+        post.userLikes.add(request.user)
+        post.save()
+    return redirect("/")
+
+def dislikePost(request, pk):
+    post = Post.objects.get(pk=pk)
+
+    if post.userLikes.contains(request.user):  
+        post.likes -= 1
+        post.userLikes.remove(request.user)
+        post.save()
+    return redirect("/")
+
+def addComment(request, postId):
+    post = Post.objects.get(id=postId)
+    # user = request.user.email
+    user = 'guest@buffalo.edu'
+    comment = request.POST["comment"]
+
+    postComment = Comments.objects.create(post=post, user=user, comment=comment)
+    postComment.save()
+    return redirect("/")
+
+def deleteComment(request, commentId):
+    comment = Comments.objects.get(commentId)
+    
+    if comment.user == request.user:
+        comment.delete()
+    return redirect("/")
--- evergreen/evergreen/urls.py
+++ evergreen/evergreen/urls.py
@@ -21,13 +21,19 @@ from eg_app import views
 from django.conf.urls.static import static
 from django.conf import settings
 
-
 #from evergreen import settings
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('',views.index,name='index'),
+    
     path('validate',views.validate,name='validate'), # type: ignore
-
-
+    
+    path('updateFeed',views.updateFeed,name="updateFeed"),
+    path('uploadPost',views.uploadPost,name="uploadPost"),
+    path('deletePost',views.deletePost,name="deletePost"),
+    path('likePost',views.likePost,name="likePost"),
+    path('dislikePost',views.dislikePost,name="dislikePost"),
+    path('addComment',views.addComment,name="addComment"),
+    path('deleteComment',views.deleteComment,name="deleteComment"),
 ]
