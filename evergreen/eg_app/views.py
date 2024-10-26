f8a833f273d2e89c144da25a09bafb58ffd0fbcb
Alex M <weirdalex0302+github@gmail.com>
2024-10-26 16:45:42 -0400

    Authentication methods & database backend (#33)
    * Account models and signup/login code
    
    * Adding routing for auth wip
    
    * fix wrong name for pwd conf
    
    * Refactor to use Django auth, add logout
    
    * remove now-unnecessary models
    
    * drop debug prints
    
    * Address feedback
    
    - Refactor register dupe check to avoid try/catch
    - Add explicit kwargs to `create_user` call in `register`
    - Make root path a global var for all `views` methods

--- evergreen/eg_app/views.py
+++ evergreen/eg_app/views.py
@@ -2,9 +2,12 @@ from django.shortcuts import render
 
 import eg_app.util.validators as val
 
-from django.http import JsonResponse
-
+from django.http import JsonResponse, HttpRequest, HttpResponseRedirect, HttpResponseBadRequest
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
 from django.views.decorators.csrf import csrf_exempt
+
+ROOT_PATH = "/"
 
 # Create your views here.
 
@@ -33,6 +36,53 @@ def validate(request):
 
 
         return JsonResponse({"valid_pass":str(valid_pass),"valid_email":str(valid_email)})
+    
+    return HttpResponseBadRequest()
 
+def register(request: HttpRequest):
+    if request.method == "POST":
+        email = request.POST.get("email", "")
+        password = request.POST.get("password", "")
+        passwordConf = request.POST.get("confirm_password", "")
 
+        # Make sure passwords match
+        if password != passwordConf:
+            return HttpResponseRedirect(ROOT_PATH)
+        
+        # Make sure email & pwd are valid
+        if not (val.validate_email(email, True) and val.validate_password(password)):
+            return HttpResponseRedirect(ROOT_PATH)
 
+        # Make sure email doesn't already exist
+        if len(User.objects.filter(email=email)) != 0:
+            return HttpResponseRedirect(ROOT_PATH)
+        
+        # Now confirmed valid, create account
+        newAcct = User.objects.create_user(username=email, email=email, password=password)
+        newAcct.save()
+
+        # TODO: Should send visible feedback to user
+
+        return HttpResponseRedirect(ROOT_PATH)
+    
+    return HttpResponseBadRequest()
+
+def login_view(request: HttpRequest):
+    if request.method == "POST":
+        email = request.POST.get("email", "")
+        password = request.POST.get("password", "")
+        
+        user = authenticate(request, username=email, password=password)
+        if user is not None:
+            login(request, user)
+        else:
+            # TODO: Should send visible feedback to user
+            pass
+
+        return HttpResponseRedirect(ROOT_PATH)
+    
+    return HttpResponseBadRequest()
+
+def logout_view(request: HttpRequest):
+    logout(request)
+    return HttpResponseRedirect(ROOT_PATH)
