2844347c659aee6b28a8698f6545ca7e6c80d750
Sumanyu Janapareddy <111717258+sumanyuj@users.noreply.github.com>
2024-10-27 17:30:56 -0400

    removed duplicate in manage.py

--- evergreen/manage.py
+++ evergreen/manage.py
@@ -17,15 +17,6 @@ def main():
         ) from exc
     execute_from_command_line(sys.argv)
 
-if __name__ == "__main__":
-    main()
-
-#!/usr/bin/env python
-"""Django's command-line utility for administrative tasks."""
-import os
-import sys
-
-
 
 
 if __name__ == "__main__":
