446f7c7a79b3f03eefaf9d21dd72a2f383dd61aa
Sumanyu Janapareddy <111717258+sumanyuj@users.noreply.github.com>
2024-10-27 17:26:56 -0400

    fixed all the weird merge stuff

--- evergreen/eg_app/tests/util/test_validators.py
+++ evergreen/eg_app/tests/util/test_validators.py
@@ -53,9 +53,4 @@ class TestValidatePassword(TestCase):
         self.assertTrue(validate_password("all the symbols `~!@#$%^&*())-=_+[]{}\\|;',./:\"<>?"))
         self.assertTrue(validate_password("a" * 12))
         self.assertTrue(validate_password("1" * 12))
-<<<<<<< HEAD
         self.assertTrue(validate_password("A" * 255))
-=======
-        self.assertTrue(validate_password("A" * 255))
-
->>>>>>> dev
