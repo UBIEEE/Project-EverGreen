ae5263b3cb6725f8019a353fd74802f3165ed8bc
Sumanyu Janapareddy <111717258+sumanyuj@users.noreply.github.com>
2024-10-27 09:12:08 -0400

    updated updateFeed() function

--- evergreen/staticfiles/js/functions.js
+++ evergreen/staticfiles/js/functions.js
@@ -1,61 +1,63 @@
 const ws = false;
-feedPost = {}
+feedPost = {};
 
 function start() {
-    document.getElementById("about").innerHTML += "<br/> We have Javascript, too! 😀"
-    document.getElementById("about").innerHTML += "<br/> This probably all gonna be replaced anyway idk"
+  document.getElementById("about").innerHTML +=
+    "<br/> We have Javascript, too! 😀";
+  document.getElementById("about").innerHTML +=
+    "<br/> This probably all gonna be replaced anyway idk";
 
-    updateFeed();
-    setInterval(updateFeed, 1000);
-}
- 
-function updateFeed() { //updates both posts and comments
-    const request = new XMLHttpRequest();
-    request.open("GET", "updateFeed");
-    request.send();
+  updateFeed();
+  setInterval(updateFeed, 1000);
 }
 
-function updatePosts_Feed(serverPost) {
-    
+function updateFeed() {
+  //updates both posts and comments
+  const request = new XMLHttpRequest();
+
+  request.onload = function () {
+    if (this.status === 200) {
+      const data = JSON.parse(this.responseText);
+      updatePosts_Feed(data.posts);
+    }
+  };
+  request.open("GET", "updateFeed");
+  request.send();
 }
+
+function updatePosts_Feed(serverPost) {}
 
 function uploadPost() {
-    const postImage = document.getElementBy("postImage")
-    const img = postImage.value
-    postImage.value = "";
+  const postImage = document.getElementBy("postImage");
+  const img = postImage.value;
+  postImage.value = "";
 
-    const postCaption = document.getElementById("postCaption")
-    const cap = postCaption.value
-    postCaption.value = "";
+  const postCaption = document.getElementById("postCaption");
+  const cap = postCaption.value;
+  postCaption.value = "";
 
-    const postJSON = {"image":img, "caption":cap}
-    request.open("POST", "uploadPost");
-    request.send(JSON.stringify(postJSON));
+  const postJSON = { image: img, caption: cap };
+  request.open("POST", "uploadPost");
+  request.send(JSON.stringify(postJSON));
 }
 
-function addPostToFeed() {
-
-}
+function addPostToFeed() {}
 
 function deletePost() {
-    const request = new XMLHttpRequest();
-    request.open("DELETE", "deletePost");
-    request.send();
+  const request = new XMLHttpRequest();
+  request.open("DELETE", "deletePost");
+  request.send();
 }
 
 function deleteComment(commentId) {
-    const request = new XMLHttpRequest();
-    request.open("DELETE", "deleteComment");
-    request.send();
+  const request = new XMLHttpRequest();
+  request.open("DELETE", "deleteComment");
+  request.send();
 }
 
-function post_HTML() {
+function post_HTML() {}
 
-}
-
-function comment_HTML() {
-
-}
+function comment_HTML() {}
 
 // function likeButton_HTML() {
 //  document.getElementsByClassName("likeButton").innerHTML = '<form action="dislikePost" method="post" enctype="application/x-www-form-urlencoded">{{post.likes}}<button id="dislike_button" onclick="dislikeButton_HTML()">Un-Like</button></label>'
@@ -65,6 +67,4 @@ function comment_HTML() {
 //  document.getElementsByClassName("likeButton").innerHTML = '<form action="likePost" method="post" enctype="application/x-www-form-urlencoded">{{post.likes}}<button id="like_button" onclick="likeButton_HTML()">Like</button></label>'
 // }
 
-function initWS() {
-
-}
+function initWS() {}
