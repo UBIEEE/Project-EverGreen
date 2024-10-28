8d2910a4a2eeb188dd564ebf0a0fb53990ffa107
Sumanyu Janapareddy <111717258+sumanyuj@users.noreply.github.com>
2024-10-27 20:29:36 -0400

    changed user string

--- evergreen/eg_app/models.py
+++ evergreen/eg_app/models.py
@@ -8,7 +8,7 @@ class Post(models.Model):
     #user = models.ForeignKey(User, on_delete=models.CASCADE)
     user = models.TextField(blank=False)
 
-    image = models.ImageField(upload_to="posts",blank=False,null=False)
+    image = models.ImageField(upload_to="posts",blank=True,null=True)
     caption = models.TextField(blank=True)
     timestamp = models.DateTimeField(auto_now_add=True)
     userLikes = models.ManyToManyField(User,blank=True)
@@ -16,7 +16,7 @@ class Post(models.Model):
     likes = models.PositiveIntegerField(default=0)
 
     def __str__(self):
-        return f"{'self.user'}: {self.caption}"
+        return f"{self.user}: {self.caption}"
 
 class Comments(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
