22cda34699942882cb81da3461a8a0c932f5f18f
Sumanyu Janapareddy <111717258+sumanyuj@users.noreply.github.com>
2024-10-27 20:26:33 -0400

    some stuff

--- evergreen/eg_app/models.py
+++ evergreen/eg_app/models.py
@@ -16,8 +16,7 @@ class Post(models.Model):
     likes = models.PositiveIntegerField(default=0)
 
     def __str__(self):
-
-        return f"{self.user.email}: {self.caption}"
+        return f"{'self.user'}: {self.caption}"
 
 class Comments(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
