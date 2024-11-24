a1bc80f280ed09aa60d74baa387972a4ec7e8b0e
Eric-Butcher <ebutcher66@gmail.com>
2024-11-24 16:13:38 -0500

    Getting models for skydome set up.

--- evergreen/skydome/models.py
+++ evergreen/skydome/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+import uuid
 
 # Create your models here.
+
+
+class NationalWeatherServiceAPIInfo(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4)
+
+
+class HourlyForecast(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4)
+    datetime = models.DateTimeField()
+
+    last_updated = models.DateTimeField(auto_now_add=True)
+    temperature_celsius = models.FloatField()
+    dew_point_celsius = models.FloatField()
+    relative_humidity_percentage = models.PositiveSmallIntegerField()
+    wind_chill_celsius = models.FloatField()
+    sky_cover_percentage = models.SmallIntegerField()
+    probability_of_precipitation = models.SmallIntegerField()
+    quantitative_precipitation_mm = models.FloatField()
---
+++ evergreen/skydome/radar.py
@@ -0,0 +1,15 @@
+def generate_water_state_graph(): ...
+
+
+def update_db_with_hourly_forecast_data():
+    # get the current link grid point link for our location, either it is cached or will need to re-send
+
+    # make a request to the grid point link
+
+    # parse the data out from that grid point link
+
+    # determine tables of information to update for each hour that we have been given
+
+    # update the database with the new information
+
+    ...
