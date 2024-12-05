8d3288bba3be0ff9a5331c57a6c5270c1aee019d
Eric-Butcher <ebutcher66@gmail.com>
2024-12-05 16:18:44 -0500

    Reigster models in admin file.

--- evergreen/relay_handler/admin.py
+++ evergreen/relay_handler/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+import relay_handler.models as models
 
 # Register your models here.
+admin.site.register(models.RelayDevice)
+admin.site.register(models.RelayUpload)
