6018644913167b915651bbd839154cf3bf2db47c
Eric Butcher <107886303+Eric-Butcher@users.noreply.github.com>
2024-12-06 14:53:39 -0500

    Feature/camera relay (#62)
    * Added ignores so my IDE doesn't yell at me.
    
    * Creared relay_handler model.
    
    * Fixed relay_request to use the request.POST variable instead.
    
    * Added command to create new RelayDevic.
    
    * Returns an error message if the relay handler failed to add timelapse to psotgres.
    
    * Fixed exception handling in relay_processor.py.
    
    * Reigster models in admin file.
    
    * URL for the timelapse.
    
    * Fixing linter and type-checking erroring.
    
    * Time lapse now has lcoation on the main page.
    
    * Added relay upload path.
    
    * Made some fixes
    
    * Fixed large files being 413'd.
    
    * Linting with make to fix bugs and error codes from flake8 about unused imports. Also added max_length to models.

---
+++ evergreen/relay_handler/relay_crypto.py
@@ -0,0 +1,51 @@
+import datetime
+import secrets
+
+import bcrypt
+
+from relay_handler.models import RelayDevice
+
+# TODO: It would probably be a good idea to add a peppering mechanism
+
+
+def register_new_relay_device(
+    call_name: str,
+    vendor: str,
+    device_type: str,
+    initial_activation: datetime.datetime = datetime.datetime.now(
+        datetime.timezone.utc
+    ),
+    expected_message_interval: datetime.timedelta | None = None,
+) -> str:
+    """Creates a new RelayDevice in the database.
+    Returns the raw password for the RelayDevice which is a url safe string.
+    """
+    salt: bytes = bcrypt.gensalt()
+    password_text: str = secrets.token_urlsafe(nbytes=64)
+    password_binary: bytes = password_text.encode(encoding="ascii")
+    bcrypted_hash: bytes = bcrypt.hashpw(password=password_binary, salt=salt)
+
+    RelayDevice.objects.create(
+        call_name=call_name,
+        vendor=vendor,
+        device_type=device_type,
+        initial_activation_datetime=initial_activation,
+        currently_active=True,
+        expected_message_interval=expected_message_interval,
+        bcrypted_hashed_authentication_token=bcrypted_hash,
+    )
+    return password_text
+
+
+def validate_raw_auth_token(device: RelayDevice, raw_token_text: str):
+    """Checks the auth token for a RelayDevice against the salted and hashed version
+    stored in the database.
+    Returns true if the token is valid for this RelayDevice AND if the RelayDevice
+    is active, and false otherwise.
+    """
+    inputted_token_binary: bytes = raw_token_text.encode(encoding="ascii")
+    validity = bcrypt.checkpw(
+        password=inputted_token_binary,
+        hashed_password=device.bcrypted_hashed_authentication_token,
+    )
+    return validity
