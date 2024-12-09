2bb5b3d670aaecf77be2b915681568c5e5a02596
Eric Butcher <107886303+Eric-Butcher@users.noreply.github.com>
2024-12-09 00:19:07 -0500

    Feature/camera relay (#66)
    Added README testing procedure for Part3

---
+++ device_uploading/device_spoofer.py
@@ -0,0 +1,40 @@
+# Use this file to simulate sending a request to the web server from a RelayDevice.
+
+import datetime
+import os
+import pathlib
+
+import requests
+
+CALL_NAME = "<call-name>"
+VENDOR = "<vendor>"
+DEVICE_TYPE = "<device-type>"
+AUTH_TOKEN = "<password>"
+
+filename = f"name.mp4"
+
+
+now = datetime.datetime.now(datetime.timezone.utc)
+current_time = int(now.timestamp())
+
+capture_full_path = pathlib.Path(os.getcwd()) / "capture" / filename
+
+data = {
+    "Call-Name": CALL_NAME,
+    "Relay-Device-Auth-Token": AUTH_TOKEN,
+    "Vendor": VENDOR,
+    "Device-Type": DEVICE_TYPE,
+    "UNIX-Timestamp": current_time,
+}
+
+with open(capture_full_path, "rb") as file:
+    files = {"file": (filename, file.read())}
+
+# url = "https://projectevergreen.live/relay-upload"
+url = "http://localhost/relay-upload"
+
+response = requests.post(url=url, data=data, files=files)
+
+print(f"Response code: {response.status_code}")
+print(f"Response content: {response.content}")
+print(f"Response reason: {response.reason}")
